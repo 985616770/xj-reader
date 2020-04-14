d702db26e053cbfbc1cc0d0880274edbd13348ca
zp <985616770@qq.com>
2020-04-14 23:12:42 +0800

    优化3

--- vue.config.js
+++ vue.config.js
@@ -8,33 +8,65 @@
 // const listData = require('./src/mock/bookList')
 // const flatListData = require('./src/mock/bookFlatList')
 
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  configureWebpack: {
-    devtool: 'source-map',
-    // performance: {
-    //   hints: 'warning',
-    //   maxAssetSize: 524288,
-    //   maxEntrypointSize: 524288
-    // }
-    externals: {
-      vue: 'Vue',
-      vuex: 'Vuex',
-      'vue-router': 'VueRouter',
-      axios: 'axios',
-      'vue-18n': 'VueI18n',
-      'vue-lazy': 'VueLazyload',
-      'web-storage-cache': 'WebStorageCache',
-      epubjs: 'ePub',
-      'vue-create-api': 'VueCreateAPI'
+  publicPath: isProduction ? './' : '/',
+  configureWebpack: config => {
+    if (isProduction) {
+      config.externals = {
+        vue: 'Vue',
+        vuex: 'Vuex',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        'vue-18n': 'VueI18n',
+        'vue-lazy': 'VueLazyload',
+        'web-storage-cache': 'WebStorageCache',
+        epubjs: 'ePub',
+        'vue-create-api': 'VueCreateAPI'
+      }
+      // 开启gzip压缩
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          algorithm: 'gzip',
+          test: /\.js$|\.html$|\.json$|\.css/,
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      )
+      // 开启分离js
+      config.optimization = {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 20000,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `npm.${packageName.replace('@', '')}`
+              }
+            }
+          }
+        }
+      }
     }
   },
-  devServer: {
-    // before(app) {
-    //   mock(app, '/book/home', homeData)
-    //   mock(app, '/book/shelf', shelfData)
-    //   mock(app, '/book/list', listData)
-    //   mock(app, '/book/flat-list', flatListData)
-    // }
-  }
+  css: {},
+  productionSourceMap: false
+  // devServer: {
+  //   // before(app) {
+  //   //   mock(app, '/book/home', homeData)
+  //   //   mock(app, '/book/shelf', shelfData)
+  //   //   mock(app, '/book/list', listData)
+  //   //   mock(app, '/book/flat-list', flatListData)
+  //   // }
+  // }
 }
